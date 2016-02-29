b2684dc372ae3abadb3965be3b692892aeab622a
nannen05 <nannen05@gmail.com>
2016-02-28 17:23:44 -0800

    added firebase user registration

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,5 @@
-var myApp = angular.module('myApp' , ['ngRoute']);
+var myApp = angular.module('myApp' , ['ngRoute', 'firebase'])
+	.constant('FIREBASE_URL' , 'https://angreg05.firebaseio.com/');
 
 myApp.config(['$routeProvider' , function($routeProvider) {
 	$routeProvider
--- app/js/controllers/registration.js
+++ app/js/controllers/registration.js
@@ -1,12 +1,23 @@
-myApp.controller('RegistrationController' , ['$scope', function($scope) {
+myApp.controller('RegistrationController' , ['$scope' , '$firebaseAuth', 'FIREBASE_URL' ,
+	 function($scope, $firebaseAuth, FIREBASE_URL) {
 	
-	$scope.login = function() {
-		$scope.message = "Welcome" + ' ' + $scope.user.email;
-	};
+	 	var ref = new Firebase(FIREBASE_URL);
+	 	var auth = $firebaseAuth(ref);
 
-	$scope.register = function() {
-		$scope.message = "Welcome" + ' ' + $scope.user.firstname + ' ' + $scope.user.lastname;
-	};
+		$scope.login = function() {
+			$scope.message = "Welcome" + ' ' + $scope.user.email;
+		};// Login 
 
+		$scope.register = function() {
+			auth.$createUser({
+				email : $scope.user.email,
+				password : $scope.user.password
+			}).then(function(regUser) {
+				$scope.message = "Hi" + ' ' + $scope.user.firstname + ' ' + $scope.user.lastname +
+				", Thanks for registering";
+			}).catch(function(error) {
+				$scope.message = error.message;
+			}); //createUser
+		}; //register
 
-}]);
+}]); // controller
