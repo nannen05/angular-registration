1e8d39a5acbecc1bb5e9564431f1cdaebdcfea21
nannen05 <nannen05@gmail.com>
2016-02-29 21:46:11 -0800

    added in factory

--- app/js/controllers/registration.js
+++ app/js/controllers/registration.js
@@ -1,23 +1,12 @@
-myApp.controller('RegistrationController' , ['$scope' , '$firebaseAuth', 'FIREBASE_URL' ,
-	 function($scope, $firebaseAuth, FIREBASE_URL) {
-	
-	 	var ref = new Firebase(FIREBASE_URL);
-	 	var auth = $firebaseAuth(ref);
+myApp.controller('RegistrationController' , ['$scope' , 'Authentication',
+	 function($scope, Authentication) {
 
 		$scope.login = function() {
-			$scope.message = "Welcome" + ' ' + $scope.user.email;
+			Authentication.login($scope.user);
 		};// Login 
 
 		$scope.register = function() {
-			auth.$createUser({
-				email : $scope.user.email,
-				password : $scope.user.password
-			}).then(function(regUser) {
-				$scope.message = "Hi" + ' ' + $scope.user.firstname + ' ' + $scope.user.lastname +
-				", Thanks for registering";
-			}).catch(function(error) {
-				$scope.message = error.message;
-			}); //createUser
+			Authentication.register($scope.user);
 		}; //register
 
 }]); // controller
---
+++ app/js/services/authentication.js
@@ -0,0 +1,25 @@
+myApp.factory('Authentication' , ['$rootScope', '$firebaseAuth', 'FIREBASE_URL', 
+	function($rootScope, $firebaseAuth, FIREBASE_URL) {
+
+		var ref = new Firebase(FIREBASE_URL);
+	 	var auth = $firebaseAuth(ref);
+
+	 	return {
+	 		login: function(user) {
+	 			$rootScope.message = "Welcome" + ' ' + user.email;
+	 		},
+
+	 		register: function(user) {
+		 		auth.$createUser({
+					email : user.email,
+					password : user.password
+					}).then(function(regUser) {
+						$rootScope.message = "Hi" + ' ' + user.firstname + ' ' + user.lastname +
+						", Thanks for registering";
+					}).catch(function(error) {
+						$rootScope.message = error.message;
+				}); //createUser
+	 		} //register
+	 	};
+
+	}]); //factory 
